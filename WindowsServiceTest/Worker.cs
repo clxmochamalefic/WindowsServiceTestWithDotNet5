2b4879791feb8f744bbaabe20de623bc4d55711d
nao.a <nao.akakura@valtes-mt.co.jp>
2021-03-28 14:06:18 +0900

    create windows service

--- WindowsServiceTest/Worker.cs
+++ WindowsServiceTest/Worker.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -17,12 +18,26 @@ namespace WindowsServiceTest
             _logger = logger;
         }
 
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            return base.StopAsync(cancellationToken);
+        }
+
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
+                var client = new HttpClient();
+                var response = await client.SendAsync(new(HttpMethod.Get, @"https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json"));
+                var body = await response?.Content.ReadAsStringAsync() ?? "";
+                _logger.LogInformation("response content: " + body);
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                await Task.Delay(360000, stoppingToken);
             }
         }
     }
